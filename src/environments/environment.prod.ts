f3436829eb853d619e7fccaf52a5106b013a5f20
Imad Uddin <imad@a4aero.com>
2021-04-08 16:46:12 +0600

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9g4mAHH41AK0NgMQPS9t8SRWgqGqluaU",
+    authDomain: "angular-test-367cb.firebaseapp.com",
+    projectId: "angular-test-367cb",
+    storageBucket: "angular-test-367cb.appspot.com",
+    messagingSenderId: "700558292851",
+    appId: "1:700558292851:web:1e06a386ace440dde63706",
+    measurementId: "G-75FYRVJQHL"
+  }
 };
